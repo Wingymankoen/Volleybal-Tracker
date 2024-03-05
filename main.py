a132a4c33fb5b2ab666aea3499b2a4882825ae6f
Wingymankoen <koenrvleugels@gmail.com>
2024-03-05 22:40:45 +0100

    Add data section

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
+import os
+
+from data import extract_images, import_images
+
 
 def main():
-    print('volleyball')
+    pathin = os.getcwd() + '\\Data\\' + 'Vball_vid.mp4'
+    pathout = os.getcwd() + '\\Data\\jpeg'
+    if not (os.listdir(pathout)):
+        extract_images(pathin, pathout)
+    init_images = import_images(pathout)
 
 
 if __name__ == "__main__":
