129492cbb11ec40823c94e6b7b19b1e47f87a305
Wingymankoen <koenrvleugels@gmail.com>
2024-03-13 23:19:27 +0100

    Find ball updated

--- findball.py
+++ findball.py
@@ -3,16 +3,35 @@ import cv2 as cv
 
 def get_high(mask):
     cnts, _ = cv.findContours(mask, cv.RETR_CCOMP, cv.CHAIN_APPROX_SIMPLE)
+    chigh = None
+    cy = 10000
+    for c in cnts:
+        x, y, w, h = cv.boundingRect(c)
+        s = min(w, h)
+        if s < 15:
+            continue
 
+        if y < cy:
+            r = max(w, h) / s
+            if r > 1.5:
+                continue
+
+            cy = y
+            chigh = c
+    return chigh
 
 def get_ball(figs, wd):
 
-    for frame in figs:
+    for idx, frame in enumerate(figs):
         mask = cv.GaussianBlur(frame, (7, 7), 0)
         mask = cv.cvtColor(mask, cv.COLOR_BGR2GRAY)
         ret, mask = cv.threshold(mask, 0, 255, cv.THRESH_BINARY | cv.THRESH_OTSU)
         #cmask = cv.cvtColor(mask, cv.COLOR_GRAY2BGR)
         chigh = get_high(mask)
+        if chigh is not None:
+            rx, ry, rw, rh = cv.boundingRect(chigh)
+            cut = mask[ry: ry + rh, rx: rx + rw]
+            cv.imwrite(wd + '\\Data\\Contours' + "\\cleared%d.jpg" % idx, cut)
 
         #cv.imwrite(wd + "\\temp.jpg", mask)
 
